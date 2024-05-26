190269a89743f4ca2487d7d12daf600ad7ee135a
noleemits <45466598+noleemits@users.noreply.github.com>
2024-05-26 12:35:31 -0600

    Create config.php
    Added config with constants

---
+++ includes/config.php
@@ -0,0 +1,9 @@
+<?php
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
+
+define('CSB_LAWYER_CATEGORY_SLUG', 'tipos-de-abogados');
+define('CSB_RESULTS_PAGE', 'lawyer-search-results');
+define('CSB_METRO_TAX', 'metro');
